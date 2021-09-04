a43a17d15bf991ccc0db7d7c8629d08ee62604d8
Anton Nedogarok <nk260an@gmail.com>
2021-09-04 16:20:04 +0300

    Added ubuntu dependencies

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+sudo apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr
+sudo apt-get install flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev
+sudo apt-get install python3-pip
+pip3 install textract
+
+pip3 install PIL
+pip3 install pytesseract
+pip3 install pdf2image
+sudo apt-get install tesseract-ocr
