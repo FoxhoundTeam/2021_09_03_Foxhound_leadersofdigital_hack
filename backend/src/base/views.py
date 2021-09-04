a6cb414f429cdfce2cbed16613c4f42d06e13870
kirill_semennikov <some@mail.ru>
2021-09-04 17:17:11 +0300

    format xls_processing.py

--- backend/src/base/views.py
+++ backend/src/base/views.py
@@ -1,6 +1,3 @@
-import os
-from typing import Tuple
-
 from src.base.utils import send_to_API
 from rest_framework.response import Response
 from rest_framework import viewsets
@@ -8,29 +5,13 @@ from rest_framework.decorators import action
 from src.base.models import Setting
 from src.base.serializers import RecognizeSerializer, SettingSerializer
 
-from backend.src.base.xls_processing import analyze_xls
+from backend.src.base.utils import process_doc
 
 MIME_TYPES = {
     'pdf': 'application/pdf',
     'xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     'xls': 'application/vnd.ms-excel',
 }
-
-
-def process_doc(file, ext, setting) -> Tuple[str, str, str]:
-    try:
-        if ext in ("xlsx", 'xls'):
-            code = analyze_xls(file, setting)  # Кирилл
-        elif ext == "pdf":
-            code = '1'
-            # code = analyze_pdf(filename) # Антон Н.
-        else:
-            return "None", "None", "Загрузите документ с расширением xls/xlsx или pdf"
-    except Exception as ex:
-        return "None", "None", f"Ошибка в процессе обработки {ex}"
-    # recommended_name = get_recommended_name(filename, type)
-    recommended_name = ''
-    return recommended_name, code, ''
 
 
 class SettingViewSet(viewsets.ModelViewSet):
