c1ac3428e53829bbb02c2e51c67aee0503f7000f
anton <antonp0925@gmail.com>
2021-09-05 04:07:58 +0300

    fixed dependences

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-apt-get -y install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr
+apt-get -y install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils tesseract-ocr
 apt-get -y install flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev
 apt-get -y install python3-pip
